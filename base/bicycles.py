cdeb5533d7195d780cf16f3c98ab9faa6e88a486
Alex Lamizana <lamizana1987@yahoo.fr>
2024-06-19 18:04:11 +0200

    update 19 juin -liste-

--- base/bicycles.py
+++ base/bicycles.py
@@ -1,7 +1,9 @@
 # cours_python/base/bicycles.py
 ###############################################################################
+#                                                                             #
 # Présentation des listes.      source:"Petite lecon de python"               #
-# Compréhesion et gestion de bases des listes.               - 19/juin/2024 - #
+# Compréhesion et gestion de bases des listes.                                #
+#                                                            - 19/juin/2024 - #
 ###############################################################################
 
 # Initialisation et assignation d'une liste:
@@ -26,6 +28,7 @@ print(potes)
 # on peut mettre plusieurs variable dans un print.           
 print(potes[2].title(), potes[-1].title())                     
 
+
 # Créer un msg avec le nom de chacun de mes potes:
 msg_0 = f"Salutation {potes[0].title()}, comment vas-tu aujourd'hui?"
 msg_1 = f"Salutation {potes[1].title()}, comment vas-tu aujourd'hui?"
@@ -33,16 +36,19 @@ msg_2 = f"Salutation {potes[2].title()}, comment vas-tu aujourd'hui?"
 # on peut mettre des print à la suite avec ",".
 print(msg_0), print(msg_1), print(msg_2)       
 
+
 # Modifier un élément de la liste:
 # remplace l'ancienne valeur à l'index 0 par "hervé" .                    
 potes[0] = "hervé"                             
 print(potes)
+
 
 # Ajouter un élément à la fin de la liste :
 print("\n--Append()--")
 # Ajoute un pote dans la liste avec "append":
 potes.append("nicolas")                         
 print(potes)
+
 
 # insérer un élément dans la liste:
 print("\n--Insert()--")
@@ -51,10 +57,12 @@ print("\n--Insert()--")
 potes.insert(2, "marie")                        
 print(potes)                                  
 
+
 # Supprimer définitivement les éléments d'une liste:
 print("\n--Del--")
 del potes[0]                        # Efface la valeur en position 0 avec "del":
 print(potes)                        # supprime définitivement la valeur.
+
 
 # Supprimer les éléments d'une liste avec pop():
 print("\n--Pop()--")
@@ -62,15 +70,18 @@ popped_potes = potes.pop()      # Stocke la valeur supprimé dans une variable:
 print(potes)                    # Affiche les valeurs sans la variable supprimé.
 print(popped_potes)             # Affiche la valeur supprimé.  
 
+
 # Supprimer des éléments à une position donnée:
 popped_potes = potes.pop(3)     # Choisi et stocke la valeur supprimé.
 print(potes)                    # Affiche les valeurs sans la variable supprimé 
 print(popped_potes)             # Affiche la valeur supprimé.  
 
+
 # Supprimer un élément par sa valeur avec remove():
 print("\n--Remove()--")                 
 potes.remove("jack")             # Supprime l'élément indiqué par sa valeur:
 print(potes)                     # on peut stocker sa valeur comme pop().
+
 
 # ------------------------------------------------------------------------------
 # ORGANISER UNE LISTE.
@@ -83,12 +94,14 @@ print(potes)
 potes.sort(reverse=True)                       
 print(potes)
 
+
 # Trier provisoirement une liste avec sorted():
 print("\n--Sorted()--") 
 # affiche la liste initial.
 print(f"Voici la liste initial:, {potes}")  
 # classe par ordre alphabétique: provisoire.            
 print(f"Voici la liste final:, {sorted(potes)}")          
+
 
 # Afficher une liste dans l'odre inverse avec reverse():
 print("\n--Reverse()--") 
@@ -98,9 +111,11 @@ print(potes)
 potes.reverse()                    # inverse une seconde fois l'odre.
 print(potes)
 
+
 # Obtenir la longueur d'une liste avec len():
 print("\n--len()--") 
 print(len(potes))
+
 
 # ------------------------------------------------------------------------------
 # LISTE DE NOMBRES:
@@ -116,6 +131,7 @@ print(f"Valeur minimal: {min(list_nombres)}")
 print(max(list_nombres))
 # Affiche la somme des valeurs de la liste:                          
 print(sum(list_nombres))                          
+
 
 # ------------------------------------------------------------------------------
 # MANIPULER UNE PARTIE DE LISTE:
@@ -137,12 +153,14 @@ print(players[3:])
 # Affiche les deux derniers joueurs.                              
 print(players[-2:])                               
 
+
 # Créer une boucle sur une tranche:
 print("\n--Boucle sur une tranche--")
 print(f"Voici les 3 premiers joueurs:")
 # Éxecute la boucle sur les 3 premiers noms.
 for i in players[:3]:                             
     print(i.title())
+
 
 # Copier une liste avec slice:
 print("\n--Copier une liste avec slice:--")
---
+++ base/exos/noms.py
@@ -0,0 +1,24 @@
+# cours_python/base/exos/noms.py
+###############################################################################
+#                                                                             #
+#       Exercices: 3.1                        source:"Petite lecon de python" #
+#       - Gestion des listes.                                                 #
+#                                                           - 19/juin/2024 -  #
+###############################################################################
+
+###############################################################################
+# Exos 3.1: noms.
+"""
+Stocker les noms de quelques-un de vos amis dans une liste appelée "noms".
+- Afficher le nom de chaque personne en acédant à chaque élément de la liste.
+- 1 à la fois.
+"""
+print("---------------------------------------------------------------------")
+print("Exos 3.1: noms.")
+
+nom = ["nil", "mathis", "vincent", "axel", "valentin", "nathan"]
+
+
+print("---------------------------------------------------------------------")
+
+###############################################################################
